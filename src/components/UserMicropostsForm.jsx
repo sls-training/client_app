fab450c910870cfbdf34126671e031723f263e34
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-05-19 10:43:53 +0900

    引数名変更&URL分岐の記述を改善

--- src/components/UserMicropostsForm.jsx
+++ src/components/UserMicropostsForm.jsx
@@ -7,42 +7,33 @@ const UserMicropostsForm = () => {
   // ページ数
   const [pageNumberValue, setPageNumberValue] = useState("");
   // マイクロポスト表示数
-  const [postValue, setPostValue] = useState("");
+  const [micropostsValue, setmicropostsValue] = useState("");
 
   const navigate = useNavigate();
 
   // 値が送信された時のURLの分岐
-  const NavigateUrl = (userid, pageNumber, post) => {
-    // ページ数と表示個数が空欄だった場合
-    if (pageNumber === "" && post === "") {
-      return navigate(`/users/${userid}/microposts/page/1/per_page/30`);
-      // ページ数のみ空欄だった場合
-    } else if (pageNumber === "") {
-      return navigate(`/users/${userid}/microposts/page/1/per_page/${post}`);
-      // 表示個数のみ空欄だった場合
-    } else if (post === "") {
-      return navigate(
-        `/users/${userid}/microposts/page/${pageNumber}/per_page/30`
-      );
-      // どちらも入力があった場合
-    } else {
-      return navigate(
-        `/users/${userid}/microposts/page/${pageNumber}/per_page/${post}`
-      );
-    }
+  const NavigateUrl = (userid, pageNumber, micropostsNumber) => {
+    // ページが入力されていなかったら「1」、入力されていたらその値を入れる
+    pageNumber = pageNumber === "" ? "1" : pageNumber;
+    // 表示マイクロポスト数が入力されていなかったら「30」、入力されていたらその値を入れる
+    micropostsNumber = micropostsNumber === "" ? "30" : micropostsNumber;
+
+    return navigate(
+      `/users/${userid}/microposts/page/${pageNumber}/per_page/${micropostsNumber}`
+    );
   };
 
   const handleClick = () => {
     // ユーザーIDが入力されていて、空欄でない場合
     if (useridValue && useridValue.match(/\S/g)) {
-      NavigateUrl(useridValue, pageNumberValue, postValue);
+      NavigateUrl(useridValue, pageNumberValue, micropostsValue);
     }
   };
 
   const pressEnter = (e) => {
     // Enterキーを押す&ユーザーIDが入力されていて、空欄でない場合
     if (e.key === "Enter" && useridValue && useridValue.match(/\S/g)) {
-      NavigateUrl(useridValue, pageNumberValue, postValue);
+      NavigateUrl(useridValue, pageNumberValue, micropostsValue);
     }
   };
 
@@ -74,10 +65,10 @@ const UserMicropostsForm = () => {
         表示する個数を入力
         <input
           type="text"
-          value={postValue}
+          value={micropostsValue}
           placeholder="per_pageを入力(半角数値)"
           onKeyPress={pressEnter}
-          onChange={(event) => setPostValue(event.target.value)}
+          onChange={(event) => setmicropostsValue(event.target.value)}
         />
       </label>
 
