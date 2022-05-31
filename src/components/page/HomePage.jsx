38e22bfa91b46001d810535529a10e62ab0dad9f
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-05-31 16:55:08 +0900

    ローカルストレージの存在によって表示切り替え

--- src/components/page/HomePage.jsx
+++ src/components/page/HomePage.jsx
@@ -1,13 +1,26 @@
 import React from "react";
+import { Navigate } from "react-router-dom";
 
 import UserMicropostsForm from "../UserMicropostsForm";
+import LogoutButton from "../LogoutButton";
 
-const HomePage = () => {
+const Home = () => {
   return (
     <div>
+      <LogoutButton/>
       <h1>Home</h1>
       <UserMicropostsForm />
     </div>
+  );
+}
+
+const HomePage = () => {
+  return (
+    <>
+      {
+        localStorage.getItem("Flag") ? <Home /> : <Navigate to="/login" />
+      }
+    </>
   );
 };
 
