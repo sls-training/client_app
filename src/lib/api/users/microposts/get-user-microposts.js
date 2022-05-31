995e6df27846f6c8457256312518535a98a8b021
清水大典 <d_shimizu@infosysnoMacBook-Pro.local>
2022-05-31 16:41:18 +0900

    baseURL変更によるURLの変更&herder処理を追記

--- src/lib/api/users/microposts/get-user-microposts.js
+++ src/lib/api/users/microposts/get-user-microposts.js
@@ -1,5 +1,6 @@
 import client from '../../client';
+import Cookies from 'js-cookie';
 
 export const getList = (userId, pageNumber, perPage) => {
-  return client.get(`/${userId}/microposts?page=${pageNumber}&per_page=${perPage}`);
+  return client.get(`/users/${userId}/microposts?page=${pageNumber}&per_page=${perPage}`,{ headers: {'Authorization': Cookies.get("access_token")} });
 };
